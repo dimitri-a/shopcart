95128f949bbb2f62ab25fb48a2408151efbc0788
Dimitri C <couwenberg@gmail.com>
2018-08-30 16:10:51 +1000

    cart reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,1 @@
-const initialState = 0;
-
-export default (state = initialState, action) => {
-  switch (action.type) {
-
-  case 'INCREMENT':
-  debugger;
-    return state+1;
-
-  default:
-    return state
-  }
-}
+combine reducer sometihing
